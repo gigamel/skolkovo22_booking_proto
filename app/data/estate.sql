6747fada9b4af2cf4001ba264e88aa98cba5ca85
Aleksey Shapovalov <gigamel@yandex.ru>
2024-05-01 21:42:54 +0300

    [main] add fixtures

--- app/data/estate.sql
+++ app/data/estate.sql
@@ -6,3 +6,16 @@ CREATE TABLE IF NOT EXISTS `estate` (
     `price` INT(10) NOT NULL,
     `currency` CHAR(3) NOT NULL
 );
+
+INSERT INTO `estate` (`title`, `summary`, `type`, `price`, `currency`)
+VALUES
+("Cottage number One", "This is the best cottage", "cottage", 500000, "RUB"),
+("Room of hostel", "Let receive this room", "room", 300000, "RUB"),
+("Daily Flat", "Flat for one day", "flat", 250000, "RUB"),
+("Pavilion for barbeque", "", "Nice choice", 50000, "RUB"),
+("Big house of countryside", "Just look at this miracle", "house", 200000, "RUB"),
+("Cottage number Two", "Cottage of two stars", "cottage", 350000, "RUB"),
+("Pavilion on the river bank", "Enjoy the wonderful atmosphere and views", "pavilion", 75000, "RUB"),
+("Garage for only big cars", "Trucks are welcome", "garage", 1500000, "RUB"),
+("Flat on the second floor", "Come here alone", "flat", 750000, "RUB"),
+("House for your family", "Spend time with the whole family together", "house", 420000, "RUB");
